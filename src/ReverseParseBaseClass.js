9e1709be5c8651c1993dd9a9b2db4b166312e330
Mohit Sorde <MohitS@talentica-all.com>
2019-04-14 08:44:41 +0530

    reverse deep array

--- src/ReverseParseBaseClass.js
+++ src/ReverseParseBaseClass.js
@@ -14,17 +14,26 @@ function ReverseParserBaseClass (params, attrDefs, getter, asyncHandle) {
   GenericParserClass.apply(this, arguments)
 }
 
+function _handleDeepArray (inputArr, cb) {
+  let outputArr = []
+  for (let elem of inputArr) {
+    if (!Array.isArray(elem)) outputArr.push(cb(elem))
+    else if (!elem.length) outputArr.push([])
+    else outputArr.push(this._handleDeepArray(elem, cb))
+  }
+  return outputArr
+}
+
 function _handleParser (paramObj, ParserClassArg) {
   if (Array.isArray(ParserClassArg)) {
     ParserClassArg = ParserClassArg[0]
-    let parsedArr = []
     if (!Array.isArray(paramObj)) {
       paramObj = [paramObj]
     }
-    for (let elem of paramObj) {
-      parsedArr.push((new ParserClassArg()).getReverseParams(elem))
-    }
-    return parsedArr
+    return this._handleDeepArray(
+      paramObj,
+      elem => (new ParserClassArg()).getReverseParams(elem)
+    )
   }
   return (new ParserClassArg()).getReverseParams(paramObj)
 }
@@ -36,8 +45,12 @@ function _parseParams (params) {
     let attrDef = this.attrDefs[attrKey]
     let key = attrDef['outKey'] || attrKey
     if (!params[key]) continue
-    if (Array.isArray(attrDef) && Array.isArray(params[key])) parsedObj[key] = params[key].map(elem => this.getter.exec(elem, attrDef[0]['getters']))
-    else if (attrDef['parser']) parsedObj[key] = this._handleParser(params[key], attrDef['parser'])
+    if (Array.isArray(attrDef) && Array.isArray(params[key])) {
+      parsedObj[key] = this._handleDeepArray(
+        params[key],
+        elem => this.getter.exec(elem, attrDef[0]['getters'])
+      )
+    } else if (attrDef['parser']) parsedObj[key] = this._handleParser(params[key], attrDef['parser'])
     else if (!attrDef['getters']) continue
     else parsedObj[key] = this.getter.exec(params[key], attrDef['getters'])
   }
@@ -45,18 +58,34 @@ function _parseParams (params) {
   return parsedObj
 }
 
+async function _asyncHandleDeepArray (inputArr, cb) {
+  let outputArr = []
+  for (let elem of inputArr) {
+    if (!Array.isArray(elem)) {
+      let parsedObj = await cb(elem)
+      outputArr.push(parsedObj)
+    } else if (!elem.length) outputArr.push([])
+    else {
+      let parsedObj = await this._handleDeepArray(elem, cb)
+      outputArr.push(parsedObj)
+    }
+  }
+  return outputArr
+}
+
 async function _asyncHandleParser (paramObj, ParserClassArg) {
   if (Array.isArray(ParserClassArg)) {
     ParserClassArg = ParserClassArg[0]
-    let parsedArr = []
     if (!Array.isArray(paramObj)) {
       paramObj = [paramObj]
     }
-    for (let elem of paramObj) {
-      let revParsedObj = await (new ParserClassArg()).getReverseParams(elem, true)
-      parsedArr.push(revParsedObj)
-    }
-    return parsedArr
+    return this._asyncHandleDeepArray(
+      paramObj,
+      async elem => {
+        let revParsedObj = await (new ParserClassArg()).getReverseParams(elem, true)
+        return revParsedObj
+      }
+    )
   }
   return (new ParserClassArg()).getReverseParams(paramObj, true)
 }
@@ -68,11 +97,13 @@ async function _asyncParseParams (params) {
     let key = attrDef['outKey'] || attrKey
     if (!params[key]) continue
     if (Array.isArray(attrDef) && Array.isArray(params[key])) {
-      parsedObj[key] = []
-      for (let elem of params[key]) {
-        let val = await this.getter.asyncExec(elem, attrDef[0]['getters'])
-        parsedObj[key].push(val)
-      }
+      parsedObj[key] = await this._asyncHandleDeepArray(
+        params[key],
+        async elem => {
+          let val = await this.getter.asyncExec(elem, attrDef[0]['getters'])
+          return val
+        }
+      )
     } else if (attrDef['parser']) parsedObj[key] = await this._asyncHandleParser(params[key], attrDef['parser'])
     else if (!attrDef['getters']) continue
     else parsedObj[key] = await this.getter.asyncExec(params[key], attrDef['getters'])
@@ -88,6 +119,8 @@ ReverseParserBaseClass.prototype = Object.assign(ReverseParserBaseClass.prototyp
   _handleParser,
   _asyncParseParams,
   _asyncHandleParser,
+  _handleDeepArray,
+  _asyncHandleDeepArray,
   attrDefs: {},
   getter: new GetterBaseClass()
 })
